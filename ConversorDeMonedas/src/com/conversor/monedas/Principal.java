f531e8bfb0477218910a9cce3c45a91cff5a9b02
mario cortez <45076415+mariocortezBEST@users.noreply.github.com>
2025-06-05 15:04:53 -0300

    Update Principal.java
    se dejo lista la clase principal para que reemplacen por su propia api key

--- ConversorDeMonedas/src/com/conversor/monedas/Principal.java
+++ ConversorDeMonedas/src/com/conversor/monedas/Principal.java
@@ -4,7 +4,7 @@ import com.conversor.monedas.presentation.console.ConversorDeMonedas;
 public class Principal {
     public static void main(String[] args) {
         // IMPORTANTE: Reemplaza "YOUR_API_KEY" con tu clave real de ExchangeRate-API
-        String apiKey = "fe7ce0f2e7765cef4e91c5f6";
+        String apiKey = "your-API_key";
 
         ConversorDeMonedas conversor = new ConversorDeMonedas(apiKey);
         conversor.iniciar();
